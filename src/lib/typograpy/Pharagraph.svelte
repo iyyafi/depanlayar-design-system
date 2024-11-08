c4ffd890ccf6ad1d866b95f42ed8795a89749703
Iyyafi Qolbi <iyyafi@gmail.com>
2024-11-08 17:17:12 +0700

    add custom component example

---
+++ src/lib/typograpy/Pharagraph.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	let { children }: { children: Snippet } = $props();
+</script>
+
+<p class="text-2xl text-green-700">{@render children()}</p>
---
+++ src/stories/Paragraph.stories.svelte
@@ -0,0 +1,16 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import Pharagraph from '../lib/typograpy/Pharagraph.svelte';
+
+	// More on how to set up stories at: https://storybook.js.org/docs/writing-stories
+	const { Story } = defineMeta({
+		title: 'Example/Paragraph',
+		component: Pharagraph,
+		tags: ['autodocs'],
+		argTypes: {},
+		args: {}
+	});
+</script>
+
+<!-- More on writing stories with args: https://storybook.js.org/docs/writing-stories/args -->
+<Story name="Default">ASDASD</Story>
